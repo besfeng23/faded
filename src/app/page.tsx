a21457eee340409d2642a876672618a0639275ba
lawa elem <unclejose0@gmail.com>
2025-08-27 16:49:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="font-sem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,10 +137,10 @@ export default function Home() {
                 <div className="flex items-center mb-4">
                   <Avatar>
                     <AvatarImage src="" alt="User 2" />
-                    <AvatarFallback>MS</AvatarFallback>
+                    <AvatarFallback>TJ</AvatarFallback>
                   </Avatar>
                   <div className="ml-4">
-                    <p className="font-semibold">Mike Smith</p>
+                    <p className="font-semibold">Tito J</p>
                      <div className="flex text-primary">
                       <Star /><Star /><Star /><Star /><Star />
                     </div>
